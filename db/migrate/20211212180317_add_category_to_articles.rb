755bddc98356c0cbb3622a77366a8a7d76f135c4
Touhidul Islam <touhidul.islam@justice.gov.uk>
2021-12-12 20:56:25 +0000

    Add articles show action

---
+++ db/migrate/20211212180317_add_category_to_articles.rb
@@ -0,0 +1,6 @@
+class AddCategoryToArticles < ActiveRecord::Migration[6.1]
+  def change
+    add_column :articles, :category, :string
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
