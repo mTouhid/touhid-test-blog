e33287d72452329364f25096e11d8413972152f2
Touhidul Islam <touhidul.islam@justice.gov.uk>
2021-12-15 17:23:56 +0000

    Add user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -10,6 +20,16 @@ class UsersController < ApplicationController
       redirect_to root_path
     else
       render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was updated successfully."
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   has_many :articles
-  has_secure_password
 
   before_save :make_email_downcase
 
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum:3, maximum:25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {maximum:105}, format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
 
   private
 
